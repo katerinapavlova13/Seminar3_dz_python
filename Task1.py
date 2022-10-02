6971831a3395f6553e92ab7179739dbcdcf91ef5
EkaterinaPavlova <malinakata@gmail.com>
2022-10-02 13:36:13 +0400

    Save2 Task1.py

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-# 1.Задайте список из нескольких чисел.
+# 1. Задайте список из нескольких чисел.
 # Напишите программу,которая найдёт сумму элементов списка, стоящих на нечётной позиции.(не индексах)
 # Пример: - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 2, 5 и 3, ответ: 10
 
@@ -12,14 +12,28 @@ def mix_list (list):
     return list
 
 #на нечетных позизиях(четных индексах)
-def sum_number(list):
+def sum_elem_list(list):
     sum_elem = 0
-    for i in range(0, len(list), 2):
-        sum_elem += list[i]
-# на четных позициях(нечетных индексах)
-    #for i in range(1, len(list), 2):
-        #sum_elem += list[i]
+    for i in range(len(list)):
+        if i % 2 == 0:
+            sum_elem += list[i]
     return sum_elem
 
 print(mix_list(list))
-print(sum_number(list))
+print(f"Сумма элементов на нечетных позициях: {sum_elem_list(list)}")
+
+#2вариант
+#на нечетных позициях(четных индексах)
+
+# def sum_elem_list(list):
+#     sum_elem = 0
+#     for i in range(0, len(list), 2):
+#         sum_elem += list[i]
+
+# на четных позициях(нечетных индексах)
+
+    # for i in range(1, len(list), 2):
+    #     sum_elem += list[i]
+    #
+    # return sum_elem
+
